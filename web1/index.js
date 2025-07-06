280319c920ee291a38cc1d097404bbec5972e786
Ubuntu <ubuntu@ip-172-31-3-139.ap-south-1.compute.internal>
2025-07-06 05:49:02 +0000

    Initial commit- Devops project

---
+++ web1/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello from Web Server 1')
+})
+
+app.listen(3000, () => console.log('Server running on 3000'))
+
